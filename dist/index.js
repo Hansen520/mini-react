6c2b3bdc7bb479479968d005b90d99668c46ebc3
H <744769098@qq.com>
2024-04-07 17:51:46 +0800

    feat:dist

--- dist/index.js
+++ dist/index.js
@@ -2,11 +2,16 @@
 const { render, useState, useEffect } = window.MiniReact;
 function App() {
     const [count, setCount] = useState(0);
-    function handleClick() {
-        setCount((count) => count + 1);
-    }
-    return (MiniReact.createElement("div", null,
-        MiniReact.createElement("p", null, count),
-        MiniReact.createElement("button", { onClick: handleClick }, "\u52A0\u4E00")));
+    //   function handleClick(){
+    //     setCount((count)=> count + 1)
+    //   }
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCount((count) => count + 1);
+        }, 1000);
+        return () => clearTimeout(timer);
+    }, []);
+    return MiniReact.createElement("div", null,
+        MiniReact.createElement("p", null, count));
 }
-render(MiniReact.createElement(App, null), document.getElementById("root"));
+render(MiniReact.createElement(App, null), document.getElementById('root'));
